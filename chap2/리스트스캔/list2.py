0820ff15c481f8d6cfb761b653e9244a2dca6b39
Nam Jueun <45748632+namjueun@users.noreply.github.com>
2021-08-04 22:56:22 +0900

    Update list2.py

--- chap2/리스트스캔/list2.py
+++ chap2/리스트스캔/list2.py
@@ -1,1 +1,8 @@
+## 리스트 스캔 2 : 인덱스와 원소를 짝지어 enumerate() 함수로 반복해서 꺼냅니다.
+# 리스트의 모든 원소를 enumerate() 함수로 스캔 ➞ enumerate() 함수는 인덱스와 원소를 짝지어 튜플로 꺼내는 내장 함수
+
+x = ['John', 'George', 'Paul', 'Ringo']
+
+for i, name in enumerate(x):
+    print(f'x[{i}] = {name}')
 
