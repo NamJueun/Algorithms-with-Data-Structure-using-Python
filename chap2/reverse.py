399f2543b97a93535fefcea4b294729eca96757f
Nam Jueun <45748632+namjueun@users.noreply.github.com>
2021-08-09 16:17:40 +0900

    Update reverse.py

--- chap2/reverse.py
+++ chap2/reverse.py
@@ -1,4 +1,4 @@
-## 배열 원소를 역순으로 정렬하기 
+## 배열 원소를 역순으로 정렬하기
 #뮤터블 시퀀스 원소를 역순으로 정렬
 
 from typing import Any, MutableSequence
@@ -6,7 +6,8 @@ from typing import Any, MutableSequence
 def reverse_array(a: MutableSequence) -> None:
     """뮤터블 시퀀스 a의 원소를 역순으로 정렬"""
     n = len(a)
-    for i in range(n//2):    ## 교환 획수는 원소 수 // 2번. // 기호를 사용하여 나누기 연산을 한 후 소수점 이하는 버림.
+    for i in range(n//2): ## 교환 획수는 원소 수 // 2번. // 기호를 사용하여 나누기 연산을 한 후 소수점 이하는 버림.
+                          ## 원소 수가 홀수인 경우에는 가운데 원소는 교환할 필요 없음
         a[i],a[n-i-1] = a[n-i-1],a[i]
 
 if __name__ == '__main__':
