d95f3e4bf35cbfcf5063020f3cca88395aec26ad
Nam Jueun <45748632+NamJueun@users.noreply.github.com>
2021-08-14 19:11:23 +0900

    Update bsearch_verbose.py
    출력결과 더 명확하게 표시되도록 표현식 수

--- chap3/bsearch_verbose.py
+++ chap3/bsearch_verbose.py
@@ -1,5 +1,4 @@
 # 이진 검색 알고리즘의 실행 과정을 출력
-
 from typing import Any, Sequence
 
 def bin_search(a: Sequence, key: Any) -> int:
@@ -7,31 +6,36 @@ def bin_search(a: Sequence, key: Any) -> int:
     pl = 0              # 검색 범위 맨 앞 원소의 인덱스
     pr = len(a) - 1     # 검색 범위 맨 끝 원소의 인덱스
 
-    print(' |', end = '')
+###### new
+
+    print('   |', end = '')
     for i in range(len(a)):
         print(f'{i : 4}', end='')
     print()
     print('---+' + (4 * len(a) + 2) * '-')
 
+######
+
     while True:
         pc = (pl + pr) // 2     # 중앙 원소의 인덱스
 
-        print(' |', end='')
+###### new
+
+        print('   |', end='')
         if pl != pc:        # pl 원소 위에 <- 를 출력
             print((pl * 4 + 1) * ' ' + '<-' + ((pc - pl) * 4) * ' ' + '+', end='')
         else:
-            print((pc * 4 + 1) * '-' + '<+', end = '')
+            print((pc * 4 + 1) * ' ' + '<+', end='')
         if pc != pr:        # pr 원소 위에 ->를 출력
             print(((pr - pc) * 4 - 2) * ' ' + '->')
         else:
             print('->')
-        print(f'{pc:3}|', end = '')
-
+        print(f'{pc : 3}|', end = '')
         for i in range(len(a)):
-            print(f'{a[i]:4}', end = '')
+            print(f'{a[i] : 4}', end = '')
         print('\n   |')
 
-###### 중복
+######
 
         if a[pc] == key:
             return pc           # 검색 성공
