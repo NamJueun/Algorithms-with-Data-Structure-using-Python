33c77e9277a5418866413236c67e75c0f11ee967
NamJueun <namjueun1963@gmail.com>
2021-08-15 17:35:36 +0900

    Update chained_hash_test.py
    오타수정

--- chap3/chained_hash_test.py
+++ chap3/chained_hash_test.py
@@ -26,7 +26,7 @@ while True:
             print('추가를 실패했습니다.!')
 
     elif menu == Menu.삭제:      # 삭제
-        key = int(input('추가할 키를 입력하세요.: '))
+        key = int(input('삭제할 키를 입력하세요.: '))
         if not hash.remove(key):
             print('삭제를 실패했습니다.')
 
