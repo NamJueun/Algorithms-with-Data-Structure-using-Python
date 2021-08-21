e7a5afc925bfa4fd930ee00d60f022487bfea71d
NamJueun <namjueun1963@gmail.com>
2021-08-21 23:56:05 +0900

    Update fixed_queue_test.py
    코드 오타 수정

--- chap4/fixed_queue_test.py
+++ chap4/fixed_queue_test.py
@@ -23,11 +23,18 @@ while True:
     if menu == Menu.인큐:       # 인큐
         x = int(input('인큐할 데이터를 입력하세요.: '))
         try:
-            q.queue(x)
+            q.enque(x)
         except FixedQueue.Full:
             print('큐가 가득 찼습니다.')
 
     elif menu == Menu.디큐:       # 디큐
+        try:
+            x = q.deque()
+            print(f'디큐한 데이터는 {x}입니다.')
+        except FixedQueue.Empty:
+            print('큐가 비어 있습니다.')
+
+    elif menu == Menu.피크:       # 피크
         try:
             x = q.peek()
             print(f'피크한 데이터는 {x}입니다.')
@@ -37,7 +44,7 @@ while True:
     elif menu == Menu.검색:       # 검색
         x = int(input('검색할 값을 입력하세요.: '))
         if x in q:
-            print(f'{q.count(x)}개 포함되고, 맨 앞의 위티는 {q.find(x)}입니다.')
+            print(f'{q.count(x)}개 포함되고, 맨 앞의 위치는 {q.find(x)}입니다.')
         else:
             print('검색값을 찾을 수 없습니다.')
 
